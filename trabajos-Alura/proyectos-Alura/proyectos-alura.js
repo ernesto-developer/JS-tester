9a7f6eaa2e48fa7eec12fa1f946b6b61fd12b6a5
ERE <ernesto_711@live.com.mx>
2022-01-20 00:04:02 -0600

    se creo la funcion para hacer funcionar el copy

--- trabajos-Alura/proyectos-Alura/proyectos-alura.js
+++ trabajos-Alura/proyectos-Alura/proyectos-alura.js
@@ -4,73 +4,74 @@ var botosDesencriptar = document.querySelector("#btn-desencriptar");
 var mensajeText = document.querySelector("#msg");
 var regExp = /^[a-z\s]+$/;
 var inputText = document.querySelector("#input-texto");
-
-inputText.addEventListener('keyup',() =>{
-    
-
-if(regExp.test(inputText.value)){
-    console.log("si hay minusculas")
-}else{
-    console.log("no hay minusculas");
-    console.log(inputText.value);
-}
+var inputMensaje = document.getElementById("msg");
+var botonCopiar = document.getElementById("btn-copy");
 
 
+botonCopiar.addEventListener('click',function(){
+    inputMensaje.focus();
+    document.execCommand('selectAll');
+    document.execCommand('copy');
+    // navigator.clipboard.writeText(inputMensaje.value);
 })
 
+inputText.addEventListener("keyup", () => {
+  if (regExp.test(inputText.value)) {
+    console.log("si hay minusculas");
+  } else {
+    console.log("no hay minusculas");
+    console.log(inputText.value);
+  }
+});
+
+
+
 function encriptar(wrd) {
-   if(wrd == "a"){
-       wrd = "ai";
-   }else if(wrd == "e"){
-       wrd = "enter";
-   }else if(wrd == "i"){
-       wrd = "imes";
-   }else if(wrd == "o"){
-       wrd = "ober";
-   }else if(wrd == "u"){
-       wrd = "ufat";
-   }
-    
+  if (wrd == "a") {
+    wrd = "ai";
+  } else if (wrd == "e") {
+    wrd = "enter";
+  } else if (wrd == "i") {
+    wrd = "imes";
+  } else if (wrd == "o") {
+    wrd = "ober";
+  } else if (wrd == "u") {
+    wrd = "ufat";
+  }
 
-   return wrd;
+  return wrd;
 }
 
-
-function desencriptar(wrd){
-    
-    if(wrd == "ai"){
-        wrd = "a";
-    }else if(wrd == "enter"){
-        wrd = "e";
-    }else if(wrd == "imes"){
-        wrd = "i";
-    }else if(wrd == "ober"){
-        wrd = "o";
-    }else if(wrd == "ufat"){
-        wrd = "u"
-    }
-    return wrd;
-    
+function desencriptar(wrd) {
+  if (wrd == "ai") {
+    wrd = "a";
+  } else if (wrd == "enter") {
+    wrd = "e";
+  } else if (wrd == "imes") {
+    wrd = "i";
+  } else if (wrd == "ober") {
+    wrd = "o";
+  } else if (wrd == "ufat") {
+    wrd = "u";
+  }
+  return wrd;
 }
 
+function mensajeEncriptado() {
+  var newtexto = textoIntro.value.replace(/[aeiou]/g, encriptar);
 
-function mensajeEncriptado(){
-    
-    var newtexto = textoIntro.value.replace(/[aeiou]/g, encriptar);
-
-    mensajeText.value = newtexto;
-    return false;
-
+  mensajeText.value = newtexto;
+  return false;
 }
-function mensajeDesencriptado(){
+function mensajeDesencriptado() {
+  var newtexto = textoIntro.value.replace(
+    /ai|enter|imes|ober|ufat/g,
+    desencriptar
+  );
 
-    var newtexto = textoIntro.value.replace(/ai|enter|imes|ober|ufat/g, desencriptar);
-    
-    mensajeText.value = newtexto;
-    return false;
-
+  mensajeText.value = newtexto;
+  return false;
 }
- 
- botonEncriptarClick.onclick = mensajeEncriptado;
- botosDesencriptar.onclick = mensajeDesencriptado;
 
+botonEncriptarClick.onclick = mensajeEncriptado;
+botosDesencriptar.onclick = mensajeDesencriptado;
